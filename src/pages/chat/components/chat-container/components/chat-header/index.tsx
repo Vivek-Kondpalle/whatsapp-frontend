13b333f824ad45df94206a04f6f2d390e101e1dd
vivekKondpalleOmnify <vivek@getomnify.com>
2025-01-25 22:49:48 +0530

    Create Channel and get User channels done

--- src/pages/chat/components/chat-container/components/chat-header/index.tsx
+++ src/pages/chat/components/chat-container/components/chat-header/index.tsx
@@ -11,27 +11,38 @@ const ChatHeader = () => {
       <div className="flex gap-5 items-center w-full justify-between">
         <div className="flex gap-3 items-center justify-center">
           <div className="w-12 h-12 relative">
-            <Avatar className="h-12 w-12 rounded-full overflow-hidden">
-              {selectedChatData?.image ? (
-                <AvatarImage
-                  src={`${HOST}/${selectedChatData?.image}`}
-                  alt="profile"
-                  className="object-cover w-full h-full bg-black"
-                />
-              ) : (
-                <div
-                  className={`uppercase h-12 w-12 text-lg border-[1px] flex justify-center items-center ${getColors(
-                    selectedChatData?.color
-                  )}`}
-                >
-                  {selectedChatData?.firstName
-                    ? selectedChatData?.firstName.charAt(0)
-                    : selectedChatData?.email?.charAt(0)}
+            {
+              selectedChatType === "contact" ? (
+                <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+                  {selectedChatData?.image ? (
+                    <AvatarImage
+                      src={`${HOST}/${selectedChatData?.image}`}
+                      alt="profile"
+                      className="object-cover w-full h-full bg-black"
+                    />
+                  ) : (
+                    <div
+                      className={`uppercase h-12 w-12 text-lg border-[1px] flex justify-center items-center ${getColors(
+                        selectedChatData?.color
+                      )}`}
+                    >
+                      {selectedChatData?.firstName
+                        ? selectedChatData?.firstName.charAt(0)
+                        : selectedChatData?.email?.charAt(0)}
+                    </div>
+                  )}
+                </Avatar>
+              ) :(
+                <div className="bg-[#ffffff22] h-10 w-10 flex items-center justify-center rounded-full">
+                  #
                 </div>
-              )}
-            </Avatar>
+              )
+            }
           </div>
           <div>
+            {
+              selectedChatType === "channel" && selectedChatData.name
+            }
             {
               selectedChatType === "contact" &&  (selectedChatData.firstName && selectedChatData.lastName) ? `${selectedChatData.firstName} ${selectedChatData.lastName}` : selectedChatData.email
             }
---
+++ src/pages/chat/components/contacts-container/components/create-channel/index.tsx
@@ -0,0 +1,111 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip";
+  import React, { useEffect, useState } from "react";
+  import { FaPlus } from "react-icons/fa";
+  import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+  } from "@/components/ui/dialog";
+  import { apiClient } from "@/lib/api-client";
+  import { CREATE_CHANNEL_ROUTE, GET_ALL_CONTACTS_ROUTE } from "@/utils/constants";
+  import { useAppStore } from "@/store";
+import { Button } from "@/components/ui/button";
+import MultipleSelector from "@/components/ui/multiselect";
+  
+  const CreateChannel = () => {
+    const {setSelectedChatType, setSelectedChatData, addChannel} = useAppStore()
+
+    const [newChannelModal, setNewChannelModal] = useState(false);
+    const [allContacts, setAllContacts] = useState([]);
+    const [selectedContacts, setSelectedContacts] = useState([]);
+    const [channelName, setChannelName] = useState("");
+    
+
+    useEffect(() => {
+      const getData = async () => {
+        const response = await apiClient.get(GET_ALL_CONTACTS_ROUTE, {withCredentials: true});
+        setAllContacts(response.data.contacts);
+      }
+
+      getData()
+    }, [])
+  
+    const createChannel = async () => {
+      try {
+        if(selectedContacts.length > 0 && channelName.length > 0){
+          const response = await apiClient.post(CREATE_CHANNEL_ROUTE, {
+            name: channelName,
+            members: selectedContacts.map(contact => contact.value)
+          }, {withCredentials: true});
+
+          if(response.status === 201){
+            setChannelName("");
+            setSelectedContacts([]);
+            setNewChannelModal(false);
+            addChannel(response.data.channel);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  
+    return (
+      <>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger className="p-2">
+              <FaPlus
+                className="text-neutral-400 font-light text-opacity-90 text-start hover:text-neutral-100 cursor-pointer transition-all duration-300"
+                onClick={() => setNewChannelModal(true)}
+              />
+            </TooltipTrigger>
+            <TooltipContent className="bg-[#1c1b1e] border-none mb-2 p-3 text-white">
+              Select New Contact
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <Dialog open={newChannelModal} onOpenChange={setNewChannelModal}>
+          <DialogContent className="bg-[#181920] border-none text-white w-[400px] h-[400px] flex flex-col">
+            <DialogHeader>
+              <DialogTitle>Please select a contact</DialogTitle>
+            </DialogHeader>
+            <div>
+              <input
+                type="text"
+                placeholder="Channel Name"
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none w-full"
+                onChange={(e) => setChannelName(e.target.value)}
+                value={channelName}
+              />
+            </div>
+            <div>
+              <MultipleSelector 
+                className="rounded-lg bg-[#2c2e3b] border-none py-2 text-white" 
+                defaultOptions={allContacts}
+                placeholder="Select Contacts"
+                value={selectedContacts}
+                onChange={setSelectedContacts}
+                emptyIndicator={
+                  <p className="text-center text-lg leading-10 text-gray-600">No results found</p>
+                }
+              />
+            </div>
+             <div>
+              <Button className="w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300" onClick={createChannel}>Create Channel</Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </>
+    );
+  };
+  
+  export default CreateChannel;
+  
