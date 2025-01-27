d42b1413e268b7727c4133beeb5a65ea35820bdc
vivekKondpalleOmnify <vivek@getomnify.com>
2025-01-27 22:47:49 +0530

    Types added in auth-slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,11 @@ import { create } from "zustand";
 import { createAuthSlice } from "./slices/auth-slice";
 import { createChatSlice } from "./slices/chat-slice";
 
-export const useAppStore = create()((...a) => ({
+import type { AuthSlice } from "./slices/auth-slice";
+
+type Store = AuthSlice & ReturnType<typeof createChatSlice>;
+
+export const useAppStore = create<Store>()((...a) => ({
   ...createAuthSlice(...a),
   ...createChatSlice(...a),
 }));
--- src/store/slices/auth-slice.ts
+++ src/store/slices/auth-slice.ts
@@ -1,4 +1,21 @@
-export const createAuthSlice = (set) => ({
+import { StateCreator } from "zustand";
+
+type UserInfo = {
+    id: string;
+    email: string;
+    profileSetup: boolean;
+    firstName: string;
+    lastName: string;
+    color: number;
+}
+
+export type AuthSlice = {
+    userInfo: UserInfo | undefined; 
+    setUserInfo: (userInfo: UserInfo | undefined) => void;
+};
+
+
+export const createAuthSlice: StateCreator<AuthSlice> = (set: (partial: Partial<AuthSlice>) => void): AuthSlice => ({
     userInfo: undefined,
     setUserInfo: (userInfo) => set({ userInfo })
 })
